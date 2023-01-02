b171acf1f7049df819a788e4c8215f95533be85e
id2k1149 <id2k1149@gmail.com>
2023-01-02 13:41:32 -0500

    add print textField

--- TransferData/ViewController.swift
+++ TransferData/ViewController.swift
@@ -14,6 +14,7 @@ final class ViewController: UIViewController {
     //MARK: - Private property
     
     private let label = UILabel()
+//    private let labelTwo = customLabel(text: "GoodBye", color: .blue)
     private let textfield = UITextField()
     private let button = UIButton()
 
@@ -23,6 +24,13 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         setupView()
     }
+    
+    //MARK: - Actions
+    
+    @objc
+    private func goToViewControllerTwo() {
+        print("goToViewControllerTwo")
+    }
 }
 
 //MARK: - Setup View
@@ -30,10 +38,10 @@ final class ViewController: UIViewController {
 private extension ViewController {
     
     func setupView() {
-        setupSelfView()
-        
         addViews()
+        addActions()
         
+        setupSelfView()
         setupLabel()
         setupTextField()
         setupButton()
@@ -55,18 +63,36 @@ private extension ViewController {
         view.addSubview(button)
     }
     
+    func addActions() {
+        button.addTarget(self, action: #selector(goToViewControllerTwo), for: .touchUpInside )
+    }
+    
     func setupLabel() {
         label.text = "Hello!!!"
     }
     
     func setupTextField() {
         textfield.backgroundColor = .gray
+        textfield.delegate = self
     }
     
     func setupButton() {
         button.backgroundColor = .black
         button.setTitle("GO ahead", for: .normal)
         button.tintColor = .white
+    }
+    
+    // add custom Label for each label (option 2)
+    func customLabel(text: String, color: UIColor) -> UILabel {
+        let label = UILabel()
+        
+        label.layer.cornerRadius = 10
+        label.tintColor = .red
+        
+        label.text = text
+        label.backgroundColor = color
+        
+        return label
     }
 }
 
@@ -114,5 +140,36 @@ private extension ViewController {
         ])
     }
 }
+
+// MARK: - UITextFieldDelegate
+
+
+extension ViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // must add textfield.delegate = self to func setupTextField()
+        print(textfield.text ?? "N/A")
+        
+        return true
+    }
+    
+    
+}
+
+// to add custom Label (option 1)
+/*
+final class MyLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .green
+        layer.cornerRadius = 10
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+ */
 
 
